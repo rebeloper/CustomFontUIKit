3124edad936d5ecd4f0c2463a005386efcce191f
Alex <therebeloper@gmail.com>
2018-12-03 13:26:10 +0200

    first commit

--- CustomFontUIKit/RootViewController.swift
+++ CustomFontUIKit/RootViewController.swift
@@ -7,14 +7,36 @@
 //
 
 import UIKit
+import TinyConstraints
+
+struct Fonts {
+    static let komikaTitle = "KomikaTitle"
+}
 
 class RootViewController: UIViewController {
+    
+    let label: UILabel = {
+        let label = UILabel()
+        label.text = "Custom Font"
+        label.font = UIFont(name: Fonts.komikaTitle, size: 32)
+        return label
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        view.backgroundColor = .white
+        view.addSubview(label)
+        label.centerInSuperview()
+        logAllFonts()
     }
 
+    func logAllFonts() {
+        for family in UIFont.familyNames.sorted() {
+            let names = UIFont.fontNames(forFamilyName: family)
+            print("Font Family: [\(family)] - Names: [\(names)]")
+        }
+    }
 
 }
 
