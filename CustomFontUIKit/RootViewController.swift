51be1ec4ec074f8a9fd849a044d351dcb0c39129
Alex <therebeloper@gmail.com>
2018-12-03 12:44:48 +0200

    Initial Commit

---
+++ CustomFontUIKit/RootViewController.swift
@@ -0,0 +1,20 @@
+//
+//  RootViewController.swift
+//  CustomFontUIKit
+//
+//  Created by Alex Nagy on 03/12/2018.
+//  Copyright © 2018 Alex Nagy. All rights reserved.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
